14dfdf5db3d3a0fcc1e1ac6a6b583371c368fbe7
Michael Haber <michaeladam.haber@gmail.com>
2020-07-26 17:47:45 -0400

    Ran npm install to get inquirer installed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+console.log(inquirer);
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
